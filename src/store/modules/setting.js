1cf6fb90be7a7e1b24eda289af01b82e6e6d5784
江峻 <jiangjun9@chinatowercom.cn>
2023-08-18 17:10:04 +0800

    添加srs支持

--- src/store/modules/setting.js
+++ src/store/modules/setting.js
@@ -1,11 +1,11 @@
 import md5 from 'js-md5'
+
 const state = {
-    baseUrl:"", //直播流地址最后不包含/，如http://domain/app/stream.m3u8
-    privateKey:"", //阿里云直播授权密钥
+    baseUrl: "", //直播流地址最后不包含/，如http://domain/app/stream.m3u8
+    privateKey: "", //阿里云直播授权密钥
     startTime: 0, //开始时间
     endTime: 0, //结束时间
-    latency: 0, //直播延迟，单位为秒
-    aliyunols: true //采用时移
+    latency: 0 //直播延迟，单位为秒
 }
 
 const mutations = {
@@ -29,21 +29,29 @@ const mutations = {
 }
 
 const getters = {
-    authKey (state) {
+    authKey(state) {
         const rand = 0;
         const uid = 0;
         const timeStamp = Date.now() + 1440 * 3600;
-        const pattern = /.*:\/\/.*(\/.*\/.*)/ ;
+        const pattern = /.*:\/\/.*(\/.*\/.*)/;
         const uri = state.baseUrl.match(pattern)[1];
-        return  timeStamp+'-'+rand+'-'+uid+'-'+md5(uri+'-'+timeStamp+'-'+rand+'-'+uid+'-'+state.privateKey);
+        return timeStamp + '-' + rand + '-' + uid + '-' + md5(uri + '-' + timeStamp + '-' + rand + '-' + uid + '-' + state.privateKey);
     },
-    streamUrl (state,getters) {
-        if (!state.privateKey || !state.baseUrl || !state.startTime || !state.endTime) {
-            return ""
+    streamUrl(state, getters) {
+        let url = state.baseUrl
+        if (url) {
+            if (state.privateKey) {
+                url += '?auth_key=' + getters.authKey;
+                if (state.startTime && state.endTime) {
+                    url += '&aliyunols=on&lhs_start_unix_s_0=' + (state.startTime - state.latency) + '&lhs_vodend_unix_s_0=' + (state.endTime - state.latency);
+                }
+            } else {
+                if (state.startTime && state.endTime) {
+                    url += '?start=' + (state.startTime - state.latency) + '&end=' + (state.endTime - state.latency);
+                }
+            }
         }
-        if(state.aliyunols) {
-            return state.baseUrl + '?auth_key=' + getters.authKey + '&aliyunols=on&lhs_start_unix_s_0=' + (state.startTime - state.latency) + '&lhs_vodend_unix_s_0=' + (state.endTime - state.latency);
-        }
+        return url;
     }
 }
 export default {
